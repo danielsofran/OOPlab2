f6c53d1dcb384e4c6acb8b34b9a2a5b6868d2316
Daniel <daniel.sofran@stud.ubbcluj.ro>
2022-03-19 20:18:11 +0200

    finished

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "tests.h"
 #include "ui.h"
 
+/**
+ * set(CMAKE_CXXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
+ * pentru memory leaks
+ */
+
 int main() {
     testall();
     Repository* repository = repository_create(medicament_eq);
